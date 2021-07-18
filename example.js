1a6c5c3b0f5b00e34622f1ef6086c83d4d306206
Franklin <fr@nkl.in>
2021-07-19 00:44:13 +0200

    Chore(example): Rewrite to new interface

--- example.js
+++ example.js
@@ -1,21 +1,20 @@
-const bitonic = require ('bitonic')();
+const Bitonic = require ('bitonic');
+const bitonic = new Bitonic;
 
-// Get BTC price for 10.54 BTC
-bitonic.price.buy ('eur', 10.54, (err, data) => {
-  if (err) {
-    console.error (err);
-    return;
-  }
-
-  console.log ('For 10.54 EUR you get ' + data.btc + ' BTC');
-});
+// Get BTC price for 10.54 EUR
+bitonic.priceBuy ({
+  from: 'eur',
+  amount: 10.54,
+})
+  .then (data => console.log (`For ${data.eur} EUR you get ${data.btc} BTC`))
+  .catch (console.error)
+;
 
 // Get EUR price for selling 2 BTC
-bitonic.price.sell ('btc', 2, (err, data) => {
-  if (err) {
-    console.error (err);
-    return;
-  }
-
-  console.log ('Selling 2 BTC will earn you ' + data.eur + ' EUR');
-});
+bitonic.price.sell ({
+  from: 'btc',
+  amount: 2,
+})
+  .then (data => console.log (`Selling ${data.btc} BTC will earn you ${data.eur} EUR`))
+  .catch (console.error)
+;
